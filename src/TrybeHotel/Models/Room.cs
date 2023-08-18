fafee6a8b13a318aa00091aceb4be3238f27cc15
João Lee <joaumlimaum@gmail.com>
2023-08-18 17:49:22 -0300

    Incluí alterações da model Room

--- src/TrybeHotel/Models/Room.cs
+++ src/TrybeHotel/Models/Room.cs
@@ -6,9 +6,9 @@ using System.ComponentModel.DataAnnotations.Schema;
 public class Room
 {
   public int RoomId { get; set; }
-  public string Name { get; set; }
+  public string? Name { get; set; }
   public int Capacity { get; set; }
-  public string Image { get; set; }
+  public string? Image { get; set; }
   public int HotelId { get; set; }
-  public ICollection<Hotel> Hotels { get; set; }
+  public ICollection<Hotel>? Hotels { get; set; }
 }
