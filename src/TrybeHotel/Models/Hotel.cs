cb2be7ca2733734e693d8f59d31724706610e280
João Lee <joaumlimaum@gmail.com>
2023-08-18 12:31:34 -0300

    Cria model de Room

--- src/TrybeHotel/Models/Hotel.cs
+++ src/TrybeHotel/Models/Hotel.cs
@@ -12,8 +12,3 @@ public class Hotel
   public ICollection<Room> Room { get; set; }
 
 }
-
-// HotelId: Chave primária (int)
-// Name: string
-// Address: string
-// CityId: Chave estrangeira para a model City (int) Cada hotel tem vários quartos. A propriedade de navegação para Room deve se chamar Rooms (anulável). Cada hotel tem apenas uma cidade. A propriedade de navegação para City deve se chamar City (anulável).
