0bb935682c4e8fd132b90dd2d6ed950ea28e5b06
João Lee <joaumlimaum@gmail.com>
2023-08-18 12:27:08 -0300

    Cria model de Hotel

--- src/TrybeHotel/Models/Hotel.cs
+++ src/TrybeHotel/Models/Hotel.cs
@@ -3,6 +3,17 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 // 1. Implemente as models da aplicação
-public class Hotel {
+public class Hotel
+{
+  public int HotelId { get; set; }
+  public string Name { get; set; }
+  public string Address { get; set; }
+  public int CityId { get; set; }
+  public ICollection<Room> Room { get; set; }
 
 }
+
+// HotelId: Chave primária (int)
+// Name: string
+// Address: string
+// CityId: Chave estrangeira para a model City (int) Cada hotel tem vários quartos. A propriedade de navegação para Room deve se chamar Rooms (anulável). Cada hotel tem apenas uma cidade. A propriedade de navegação para City deve se chamar City (anulável).
