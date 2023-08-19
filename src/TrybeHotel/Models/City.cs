04c9a49b239c415f4b84be6abe20e9946c7d8cfd
João Lee <joaumlimaum@gmail.com>
2023-08-19 18:38:09 -0300

    Adiciona city

--- src/TrybeHotel/Models/City.cs
+++ src/TrybeHotel/Models/City.cs
@@ -3,7 +3,6 @@ namespace TrybeHotel.Models
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
 
-    // 1. Implemente as models da aplicação
     public class City
     {
         public int CityId { get; set; }
@@ -12,5 +11,3 @@ namespace TrybeHotel.Models
 
     }
 }
-
-// City representará as cidades da aplicação e deverá conter os seguintes campos: - `CityId`: Chave primária (int) - `Name`: string Cada cidade tem vários hotéis. A propriedade de navegação para `Hotel` deve se chamar `Hotels` (anulável).
